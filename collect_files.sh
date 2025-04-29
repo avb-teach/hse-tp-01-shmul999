630b78b49477654537f46dad28a8daf7fd83642d
Никита Панченков <shmul@192.168.0.120>
2025-04-29 15:41:03 +0300

    test

--- collect_files.sh
+++ collect_files.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
 input_dir = "$1"
 output_dir = "$2"
 
 find "$input_dir" -type f | while read -r file; do
     name=$(basename "$file")
-    
     place="$output_dir/$name"
-
+    counter=1
+    while [ -e "$place" ]; do
+        DEST="output_dir/${name%.*}_$counter.${name##*.}"
+        (counter++)
+    done
     cp "$file" "$place"
 done
